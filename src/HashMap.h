cfa95faf762696ad659f1f93aa49eb8326d2c7c4
raodm <raodm@4ab50eb7-d66b-0410-b138-abf8be5d616e>
2009-05-29 18:36:03 +0000

    Fixed a bug in TransMSTClusterMaker to resolve an zero-length message issue that surfaced with testing with topspin.
    Modified the HashMap class to use more recent headers for STL hash_map.

--- src/HashMap.h
+++ src/HashMap.h
@@ -22,36 +22,60 @@
 //
 //---------------------------------------------------------------------------
 
-#ifndef _WINDOWS
+#include "config.h"
+
+#ifdef HAVE_TR1_UNORDERED_MAP
+
+// We must be compiling with gcc 4.2+ in which hash map has been moved
+// to unordered_map in preperation for the Cx0 standard compliance. So
+// use the latest and greatest standard
+#include <tr1/unordered_map>
+#define GLIBC_NAMESPACE std::tr1
+#define HashMap std::tr1::unordered_map
+#define Hash    std::tr1::hash
+
+#else // Not gcc 4.2+
+
+// Use standard hash map from the extended name space.  With GCC 3.2
+// and above the hash map data structure has been moded to a extended
+// directory under a non-std namespace.  The following defines
+// attempts to put the hash map in the standard context making it much
+// easier to work with.
+
 #include <ext/hash_map>
-#else
-#include <hash_map>
-#endif
-
-#include <string.h>
-
-#ifndef _WINDOWS
-/** With GCC 3.2 and above the hash map data structure has been moded
-    to a extended directory under a non-std namespace.  The following
-    typedef attempts to put the hash map in the standard context
-    making it much easier to work with.
-*/
+#define GLIBC_NAMESPACE __gnu_cxx
 #define HashMap __gnu_cxx::hash_map
 #define Hash    __gnu_cxx::hash
 
+#endif
+
+#include <cstring>
+
+/** String comparison structure for HashMap.
+    
+    The following structure essentially provides the functor for
+    comparison operator needed by the hash_map for comparing hash_key
+    values.  This structure specifically provides comparison for
+    hash_maps' whose key values are standard c strings.
+*/
 struct EqualStr {
     bool operator()(const char* s1, const char* s2) const {
         return strcmp(s1, s2) == 0;
     }
 };
 
-namespace __gnu_cxx {
-    template<> struct hash<std::string> {
-        inline size_t operator()(const std::string& str) const {
-            return hash<const char*>()(str.c_str());
-        }
-    };
-}       
+/** Integer comparison structure for HashMap.
+    
+    The following structure essentially provides the functor for
+    comparion operator needed by the hash_map for comparing hash_key
+    values.  This structure specifically provides comparison for
+    hash_map's whose key values are standard integers.
+*/
+struct EqualInteger {
+    inline bool operator()(const int i1, const int i2) const {
+        return (i1 == i2);
+    }
+};
 
 /** \typedef A hash_map<std::string, int>
 
@@ -62,49 +86,5 @@ namespace __gnu_cxx {
     whose key is a std::string and contains integers.
 */
 typedef HashMap<std::string, int> StringIntMap;
-
-#else
-
-// In windows the following mappings are used.
-#define HashMap stdext::hash_map
-
-/** String comparison structure for const char *.
-
-    The following structure essentially provides the comparison
-    operator needed by the hash_map for comparing hash_key values.
-    This structure specifically provides comparison for hash_map's
-    whose key values are C strings.
-*/
-struct LessString {
-    inline bool operator()(const char *s1, const char *s2) const {
-        return strcmp(s1, s2) > 0;
-    }
-};
-
-/** \typedef A hash_map<const char *, int>
-
-    A typedef for a hash map whose key is char* and contains integers.
-
-    The following typedef provides a short cut for using a hash map 
-    whose key is a C string and contains integers.
-*/
-typedef HashMap<const char *, int, stdext::hash_compare<const char *, LessString> > StringIntMap;
-
-#endif
-
-/** Integer comparison structure for std::hash_map.
-
-    The following structure essentially provides the comparison
-    operator needed by the hash_map for comparing hash_key values.
-    This structure specifically provides comparison for hash_map's
-    whose key values are standard integers.
-*/
-struct EqualInteger {
-    inline bool operator()(const int i1, const int i2) const {
-        return (i1 == i2);
-    }
-};
-
-
 
 #endif
