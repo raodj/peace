88aa47792ecf1af32a5272ab60fbe7ba0e412105
molerjc <molerjc@4ab50eb7-d66b-0410-b138-abf8be5d616e>
2010-02-24 19:58:02 +0000

    Added two more utility scripts that I had not committed to SVN but frequently use for command line gathering of statistics on clustering results.

---
+++ scripts/countSingletons.py
@@ -0,0 +1,16 @@
+import sys
+
+f1 = open(sys.argv[1], 'r')
+count = 0
+estCount = 0
+for line in f1:
+	if line[0] == 'C':
+		if estCount == 1:
+			count += 1
+		estCount = 0
+	elif line != "\n":
+		estCount += 1
+if estCount == 1:
+	count+=1
+
+print count
---
+++ scripts/getMaxClusterSize.py
@@ -0,0 +1,23 @@
+import sys
+
+f1 = open(sys.argv[1], 'r')
+maxEstCount = 0
+estCount = 0
+for line in f1:
+	if line[0] == 'C':
+		if estCount > maxEstCount:
+			maxEstCount = estCount
+		if estCount > 100:
+			print estCount
+		estCount = 0
+	elif line != "\n":
+		estCount += 1
+
+if line[0] == 'C':
+	if estCount > maxEstCount:
+		maxEstCount = estCount
+	if estCount > 100:
+		print estCount
+
+
+print maxEstCount
