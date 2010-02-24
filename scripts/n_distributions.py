82a039b5af30279b979cb6c55c34838fe0ebaf4e
molerjc <molerjc@4ab50eb7-d66b-0410-b138-abf8be5d616e>
2010-02-24 19:50:08 +0000

    Added script to obtain distributions and statistics on 'N' characters in a fasta file.

---
+++ scripts/n_distributions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+# Requires biopython to run.
+# Usage:
+# ./n_distributions [filename] [--normalize]
+# --normalize is an optional flag that will instruct
+# the script to treat any non-"ACGT" character as an N
+# (including lowercase letters and Xs).  This emulates the
+# way that Peace handles such characters.  Default is false.
+
+import sys, re
+from Bio import SeqIO
+
+
+normalize = False
+if len(sys.argv) > 2 and sys.argv[2].strip() == "--normalize":
+	normalize = True
+numBases = 0
+numN = 0
+lengthDist = dict()
+handle = open(sys.argv[1])
+for seq_record in SeqIO.parse(handle, "fasta"):
+	seq = seq_record.seq
+	seqStr = str(seq)
+	if normalize:
+		# Normalize sequence
+		seqStr = re.sub('[^ACGT]', 'N', seqStr)
+	# Get the basic counts
+	numBases += len(seqStr)
+	numN += seqStr.count('N')
+	# Add to the length distribution
+	results = re.findall('N{1,}', seqStr)
+	for pattern_match in results:
+		matchLen = len(pattern_match)
+		try:
+			lengthDist[matchLen] = lengthDist[matchLen]+1
+		except KeyError:
+			lengthDist[matchLen] = 1
+	
+handle.close()
+nPercent = float(numN) / float(numBases)
+print nPercent
+print lengthDist
