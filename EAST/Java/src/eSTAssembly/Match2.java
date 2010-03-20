d89abff6757d00d4939ea6b6511cbbaadc50eab4
zhangy9 <zhangy9@4ab50eb7-d66b-0410-b138-abf8be5d616e>
2010-03-20 03:43:28 +0000

    Modify the method for calculating A-Score.

--- EAST/Java/src/eSTAssembly/Match2.java
+++ EAST/Java/src/eSTAssembly/Match2.java
@@ -406,7 +406,7 @@ class NWSmart extends AlignSmart {
 }
 
 //Global alignment (simple gap costs, smart linear-space algorithm)
-
+/*
 class AScore extends AlignSmart {
 	int u;     // Halfway through seq1
 	int[][] c; // Best alignment from (0,0) to (i,j) passes through (u, c[i][j]) 
@@ -438,6 +438,70 @@ class AScore extends AlignSmart {
 					else if (val == F[0][j]-d)
 						c[1][j] = c[0][j];
 					else if (val == F[1][j-1]-d)
+						c[1][j] = c[1][j-1];
+					else
+						throw new Error("AScore 1");
+			}
+		}
+	}
+
+	public int getV() 
+	{ return c[1][m]; }
+
+	public String[] getMatch() {
+		int v = getV();
+		if (n > 1 && m > 1) {
+			NWSmart al1, al2;
+			al1 = new NWSmart(sub, d, seq1.substring(0, u), seq2.substring(0, v));
+			al2 = new NWSmart(sub, d, seq1.substring(u),    seq2.substring(v));
+			String[] match1 = al1.getMatch();
+			String[] match2 = al2.getMatch();
+			String[] match = { match1[0] + match2[0], match1[1] + match2[1] };
+			return match;
+		} else {
+			NW al = new NW(sub, d, seq1, seq2);
+			return al.getMatch();
+		}
+	}
+
+	public int getScore() 
+	{ return F[1][m]; }
+}
+*/
+
+class AScore extends AlignSmart {
+	int u;     // Halfway through seq1
+	int[][] c; // Best alignment from (0,0) to (i,j) passes through (u, c[i][j]) 
+
+	public AScore(Substitution sub, String sq1, String sq2) {
+		super(sub, 0, sq1, sq2);
+		int ins = 13;
+		int del = 15;
+		int n = this.n, m = this.m;
+		u = n/2;
+		c = new int[2][m+1];
+		int[][] score = sub.score;
+		for (int j=0; j<=m; j++)
+			F[1][j] = 0;
+		for (int i=1; i<=n; i++) {
+			swap01(F); swap01(c);
+			if (i == n) {
+				del = 0;
+			}
+			// F[1] represents (new) column i and F[0] represents (old) column i-1
+			F[1][0] = -ins * i;
+			for (int j=1; j<=m; j++) {
+				int s = score[seq1.charAt(i-1)][seq2.charAt(j-1)];
+				int val = max(F[0][j-1]+s, F[0][j]-ins, F[1][j-1]-del);
+				F[1][j] = val;
+				if (i == u)
+					c[1][j] = j;
+				else 
+					if (val == F[0][j-1]+s)
+						c[1][j] = c[0][j-1];
+					else if (val == F[0][j]-ins)
+						c[1][j] = c[0][j];
+					else if (val == F[1][j-1]-del)
 						c[1][j] = c[1][j-1];
 					else
 						throw new Error("AScore 1");
