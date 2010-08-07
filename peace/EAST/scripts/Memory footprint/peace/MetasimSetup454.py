063289b6828e0286f834781cbc72bd83ca55f469
zhangy9 <zhangy9@4ab50eb7-d66b-0410-b138-abf8be5d616e>
2010-08-07 08:32:35 +0000

    Readme for running test pipeline.

---
+++ peace/EAST/scripts/Memory footprint/peace/MetasimSetup454.py
@@ -0,0 +1,23 @@
+# Setup for 454
+
+import os, sys
+
+os.system('MetaSim cmd -r %s -c -4 %s' %(sys.argv[2], sys.argv[1]))
+f1 = open('%s-454.fna' %(sys.argv[1].split('.')[0]))
+f2 = open('%s-454.fa' %(sys.argv[1].split('.')[0]), 'w')
+gNum = 0
+cur = ""
+for line in f1:
+	if line[0] == '>':
+		gLabel = line.strip().split("SOURCE_1=")[1]
+		gLabel = gLabel.split(" ")[0]
+		if cur != gLabel:
+			cur = gLabel
+			gNum+=1
+		cLabel = "g%06d" %(gNum)
+		f2.write(">%s_%s" %(cLabel, line[1:]))
+	else:
+		f2.write(line)
+f1.close()
+os.system("rm %s-454.fna" %(sys.argv[1].split('.')[0]))
+f2.close()
